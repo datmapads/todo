7a0f3bf47d7e7ee15c7febbe9837d74dbc55b8f7
BroDuck1337 <uchihagasama@gmail.com>
2026-07-09 20:07:34 +0700

    Update TaskService.cpp

--- TodoApp/service/TaskService.cpp
+++ TodoApp/service/TaskService.cpp
@@ -1,8 +1,21 @@
 #include "TaskService.h"
-#include "../MySQLWrapper.h"
 
+#include <mysql.h>
 #include <iostream>
-#include "mysql.h"
+#include "..\MySQLWrapper.h"
+extern MYSQL* ConnectDB();
+
+extern std::string Escape(
+    MYSQL* conn,
+    const std::string& input
+);
+
+extern mysql_query_t p_mysql_query;
+extern mysql_store_result_t p_mysql_store_result;
+extern mysql_fetch_row_t p_mysql_fetch_row;
+extern mysql_free_result_t p_mysql_free_result;
+extern mysql_close_t p_mysql_close;
+extern mysql_error_t p_mysql_error;
 
 json TaskService::getTasks()
 {
@@ -10,16 +23,22 @@ json TaskService::getTasks()
 
     MYSQL* conn = ConnectDB();
 
-    if (!conn)
+    if(!conn)
         return arr;
 
-    if (
-        p_mysql_query(
-            conn,
-            "SELECT id,name,category,priority,completed "
-            "FROM tasks ORDER BY id"
-        )
-    )
+    const char* sql =
+    "SELECT "
+    "id,"
+    "name,"
+    "category,"
+    "priority,"
+    "due_date,"
+    "completed,"
+    "rating "
+    "FROM tasks "
+    "ORDER BY id DESC";
+
+    if(p_mysql_query(conn, sql))
     {
         std::cerr
             << "[GetTasks] "
@@ -27,27 +46,41 @@ json TaskService::getTasks()
             << std::endl;
 
         p_mysql_close(conn);
+
         return arr;
     }
 
     MYSQL_RES* res =
         p_mysql_store_result(conn);
 
-    if (res)
+    if(res)
     {
         MYSQL_ROW row;
 
-        while ((row = p_mysql_fetch_row(res)))
+        while(
+            (row =
+                p_mysql_fetch_row(res))
+        )
         {
-            arr.push_back({
-                {"id", std::stoi(row[0])},
-                {"name", row[1] ? row[1] : ""},
-                {"category", row[2] ? row[2] : ""},
-                {"priority", row[3] ? row[3] : ""},
-                {"completed",
-                    row[4] &&
-                    std::string(row[4]) == "1"}
-            });
+           arr.push_back({
+                        {"id",
+                            std::stoi(row[0])},
+                        {"name",
+                            row[1] ? row[1] : ""},
+                        {"category",
+                            row[2] ? row[2] : ""},
+                        {"priority",
+                            row[3] ? row[3] : ""},
+                        {"due_date",
+                            row[4] ? row[4] : ""},
+                        {"completed",
+                            row[5] &&
+                            std::string(row[5])=="1"},
+                        {"rating",
+                            row[6]
+                            ? std::stoi(row[6])
+                            : 0}
+                    });
         }
 
         p_mysql_free_result(res);
@@ -61,22 +94,45 @@ json TaskService::getTasks()
 bool TaskService::addTask(
     const std::string& name,
     const std::string& category,
-    const std::string& priority
+    const std::string& priority,
+    const std::string& dueDate,
+    int rating
 )
 {
     MYSQL* conn = ConnectDB();
 
-    if (!conn)
+    if(!conn)
         return false;
 
+    std::string dueValue;
+
+    if(dueDate.empty())
+    {
+        dueValue = "NULL";
+    }
+    else
+    {
+        dueValue =
+            "'" +
+            Escape(conn,dueDate) +
+            "'";
+    }
+
     std::string query =
-        "INSERT INTO tasks "
-        "(name,category,priority,completed) "
-        "VALUES ('" +
-        Escape(conn, name) + "','" +
-        Escape(conn, category) + "','" +
-        Escape(conn, priority) +
-        "',0)";
+    "INSERT INTO tasks "
+    "(name,category,priority,due_date,completed,rating) "
+    "VALUES ('" +
+    Escape(conn,name) + "','" +
+    Escape(conn,category) + "','" +
+    Escape(conn,priority) + "'," +
+    dueValue +
+    ",0," +
+    std::to_string(rating) +
+    ")";
+
+    std::cout
+        << query
+        << std::endl;
 
     int ret =
         p_mysql_query(
@@ -84,7 +140,7 @@ bool TaskService::addTask(
             query.c_str()
         );
 
-    if (ret)
+    if(ret)
     {
         std::cerr
             << "[AddTask] "
@@ -97,11 +153,13 @@ bool TaskService::addTask(
     return ret == 0;
 }
 
-bool TaskService::toggleTask(int id)
+bool TaskService::toggleTask(
+    int id
+)
 {
     MYSQL* conn = ConnectDB();
 
-    if (!conn)
+    if(!conn)
         return false;
 
     std::string query =
@@ -116,24 +174,18 @@ bool TaskService::toggleTask(int id)
             query.c_str()
         );
 
-    if (ret)
-    {
-        std::cerr
-            << "[ToggleTask] "
-            << p_mysql_error(conn)
-            << std::endl;
-    }
-
     p_mysql_close(conn);
 
     return ret == 0;
 }
 
-bool TaskService::deleteTask(int id)
+bool TaskService::deleteTask(
+    int id
+)
 {
     MYSQL* conn = ConnectDB();
 
-    if (!conn)
+    if(!conn)
         return false;
 
     std::string query =
@@ -146,14 +198,6 @@ bool TaskService::deleteTask(int id)
             conn,
             query.c_str()
         );
-
-    if (ret)
-    {
-        std::cerr
-            << "[DeleteTask] "
-            << p_mysql_error(conn)
-            << std::endl;
-    }
 
     p_mysql_close(conn);
 
